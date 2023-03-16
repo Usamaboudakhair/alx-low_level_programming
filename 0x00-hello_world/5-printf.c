da90c4029a05c5bcf654be09d559843e30e60284
Oussama Aboudakhair <usama.aboudakhair1@gmail.com>
2023-03-16 10:57:50 -0700

    Write a C program that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * writing C program that prints a lettre
+ * using printf not puts
+ * and returning 0 
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
