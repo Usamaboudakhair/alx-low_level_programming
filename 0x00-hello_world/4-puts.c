bd26e1143471dbde6e5beaa46452b3ab932a7710
Oussama Aboudakhair <usama.aboudakhair1@gmail.com>
2023-03-16 10:30:24 -0700

    Write a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void){
-puts("\"Programming is like building a multilingual puzzle");	
-return 0 ;
+
+	/* C program that prints exactly "Programming is like building a multilingual puzzl */
+
+	puts("\"Programming is like building a multilingual puzzle");	
+	
+	return 0 ;
 }
