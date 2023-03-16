31a3be3f7c54be1747f71a24d4087db7c69d1685
Oussama Aboudakhair <usama.aboudakhair1@gmail.com>
2023-03-16 11:03:41 -0700

    Write a C program that prints with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * writing C program that prints a lettre
- * using printf not puts
- * and returning 0 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
