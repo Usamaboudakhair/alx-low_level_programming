06d19d926cc2833e617b950eaae137207af680f8
Oussama Aboudakhair <usama.aboudakhair1@gmail.com>
2023-03-23 09:46:54 -0700

    This file must test that the function positive_or_negative() gives the correct output when given a case of 0.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(0);
+
+        return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,7 @@
+# 0x03. C - Debugging
+
+- 0. Multiple mains
+
+- 2. 0 > 972? This program prints the largest of three integers.
+
+- 3. Leap year
