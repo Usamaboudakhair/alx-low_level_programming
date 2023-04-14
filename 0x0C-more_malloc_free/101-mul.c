4fb62c8a6cee08f4c9eb8cdcc193bacb192610cb
Oussama Aboudakhair <usama.aboudakhair1@gmail.com>
2023-04-13 17:37:38 -0700

    101 mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -86,4 +86,45 @@ char *_multiply(char *a, char *b)
 	int i, j, k, carry;
 
 	if (res == NULL)
+		return (NULL);
+
+	_initialize_array(res, la + lb);
+
+	for (i = la - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = lb - 1, k = i + j + 1; j >= 0; j--, k--)
+		{
+			int tmp = (a[i] - '0') * (b[j] - '0') + carry + (res[k] - '0');
+			res[k] = (tmp % 10) + '0';
+			carry = tmp / 10;
+		}
+		res[k] = carry + '0';
+	}
+	while (*res == '0' && *(res + 1) != '\0')
+		res++;
+	return (res);
+}
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *res;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (_checknum(argv, 1) > 0 && _checknum(argv, 2) > 0)
+	{
+		res = _multiply(argv[1], argv[2]);
+		printf("%s\n", res);
+		free(res);
+	}
+	return (0);
 }
